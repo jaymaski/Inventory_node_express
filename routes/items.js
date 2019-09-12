2a9f097e8d931e533fc7255a3e4aedb838637619
John Joshua Macareñas <jjaymascarenas@gmail.com>
2019-09-12 09:42:56 +0800

    Added - Fetch all data from DB

--- routes/items.js
+++ routes/items.js
@@ -18,4 +18,32 @@ db.connect((err) => {
     console.log('Connected to Database!');
 });
 
+//Routes
+
+/*
+// Select posts
+router.get('/', async (req, res) => {
+    try {
+        let results = await db.all();
+        res.json(results);
+    }
+    catch(e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+*/
+
+// Select posts
+router.get('/', (req, res) => {
+    let sql = 'SELECT * FROM items';
+    let query = db.query(sql, (err, results) => {
+        if(err) throw err;
+        res.render('items.ejs', {
+            allItems: results 
+        });
+        //res.json(results);
+    });
+});
+
 module.exports = router;
