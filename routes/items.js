d662f708d9b62d4774d7df85852281e7a7617347
John Joshua Macareñas <jjaymascarenas@gmail.com>
2019-09-12 09:38:50 +0800

    Created - Connection to Database (MySQL)

--- routes/items.js
+++ routes/items.js
@@ -2,6 +2,20 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
 
+// Create connection
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'inventory'
+});
 
+// Connect
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('Connected to Database!');
+});
 
 module.exports = router;
